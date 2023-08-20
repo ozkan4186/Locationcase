ca6833e01d25554a32fb59172a68b37d060cac4f
özkan demir <ozkanndemir2009@gmail.com>
2023-08-20 17:05:27 +0300

    map added suucesfull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,43 @@
-"use client";
+"use client"
+
+
+import { useState } from "react";
 import Header from "@/components/Header";
 import List from "@/components/List";
 import Map from "@/components/Map";
 import PlaceDetail from "@/components/PlaceDetail";
 import { Flex } from "@chakra-ui/react";
 
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+interface MapProps {
+  coordinates: Coordinates;
+  setCoordinates: React.Dispatch<React.SetStateAction<Coordinates>>;
+}
+
+
 export default function Home() {
-  return <Flex 
-   justifyContent={"center"}
-   alignItems={"center"}
-   width={"100vw"}
-   height={"100vh"}
-   maxWidth={"100vw"}
-   maxHeight={"100vh"}
-   position={"relative"}
+  const [coordinates, setCoordinates] = useState<Coordinates>({
+    lat: 0,
+    lng: 0,
+  });
 
-  
-  >
-    {/* <Header/> */}
+  return (
+    <Flex
+      justifyContent={"center"}
+      alignItems={"center"}
+      width={"100vw"}
+      height={"100vh"}
+      maxWidth={"100vw"}
+      maxHeight={"100vh"}
+      position={"relative"}
+    >
+      {/* <Header /> */}
 
-    {/* <List/> */}
-    <Map/>
-  </Flex>;
+      {/* <List /> */}
+      <Map coordinates={coordinates} setCoordinates={setCoordinates} />
+    </Flex>
+  );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,27 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
-import GoogleMapReact from "google-map-react";
+import GoogleMapReact, { Coords } from "google-map-react";
 
-const Map: React.FC = () => {
+interface MapProps {
+  coordinates: Coords;
+  setCoordinates: React.Dispatch<React.SetStateAction<Coords>>;
+}
+
+const Map: React.FC<MapProps> = ({ coordinates, setCoordinates }) => {
   return (
     <Box width="full" height="full">
-      <GoogleMapReact>{/* Harita içeriği buraya gelecek */}</GoogleMapReact>
+      <GoogleMapReact
+        bootstrapURLKeys={{}}
+        defaultCenter={coordinates}
+        defaultZoom={10}
+        center={coordinates}
+        margin={[50, 50, 50, 50]}
+        options={{}}
+        onChange={() => {}}
+        onChildClick={() => {}}
+      >
+        {/* Harita içeriği buraya gelecek */}
+      </GoogleMapReact>
     </Box>
   );
 };
